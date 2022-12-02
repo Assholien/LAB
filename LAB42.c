89200d21894f08498b1401d583c8559d7578ef70
Assholien <48918951+Assholien@users.noreply.github.com>
2022-12-02 15:28:51 +0200

    Update LAB42.c

--- LAB42.c
+++ LAB42.c
@@ -274,7 +274,7 @@ void max_min(const int** A, int qrc, int** B, int rows, int cols, char qa0[100])
 {
     int A_min_max = A[0][0];
     int B_min_max = B[0][0];
-    if(strcasecmp(qa0,"max")==0)
+    if(strcasecmp(qa0,"max")==0 || strcasecmp(qa0,"maxmimum")==0)
     {
         for(int i=0; i<qrc; i++)
         {
@@ -289,7 +289,7 @@ void max_min(const int** A, int qrc, int** B, int rows, int cols, char qa0[100])
         }
         printf("B_max = %d\t", B_min_max);
     }
-    else if(strcasecmp(qa0,"min")==0)
+    else if(strcasecmp(qa0,"min")==0 || strcasecmp(qa0,"minimum")==0)
         {
         for(int i=0; i<qrc; i++)
         {
