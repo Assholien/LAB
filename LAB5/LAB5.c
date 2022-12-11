f1417a2bc9a9de5478b6c225dc6962e7b87801f6
Assholien <48918951+Assholien@users.noreply.github.com>
2022-12-11 21:57:52 +0200

    Update LAB5.c

--- LAB5/LAB5.c
+++ LAB5/LAB5.c
@@ -22,7 +22,14 @@ int main()
     {
         fscanf(fp, "%lf %lf %d %lf %s %s", &x1, &x2, &N, &delta, group, name);
         fclose(fp);
-        if (N==0) N = ((x2-x1)/delta)+1; else if (delta == 0) delta=(x2-x1)/(N-1);
+        if (delta==0 && N==0)
+        {
+            printf("Please re-evaluate your values");
+            fclose(fp_out);
+            fclose(fp_bin);
+            return 0;
+        }
+        else if (N==0) N = ((x2-x1)/delta)+1; else if (delta == 0) delta=(x2-x1)/(N-1);
         double** A = create_array(N);
         if (A==NULL) printf("\n\tMemory has not been allocated");
         else
