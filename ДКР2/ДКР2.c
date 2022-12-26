e6a95edb68743b0a6a98ce8060986c686826b822
Assholien <48918951+Assholien@users.noreply.github.com>
2022-12-26 14:11:31 +0200

    Update ДКР2.c

--- ДКР2/ДКР2.c
+++ ДКР2/ДКР2.c
@@ -6,8 +6,8 @@ int** matrix_make (int);
 void matrix_autofill (int**, int);
 void matrix_manualfill (int**, int);
 void matrix_print (const int**, int);
-void matrix_find_bigger_values_below_main_diagonal (int**, int, int*, int*);
-void matrix_print_bigger_values (int**, int, int*, int*);
+void matrix_find_bigger_values_below_main_diagonal (int**, int, int*, int*, int*, int*);
+void matrix_print_bigger_values (int**, int, int*, int*, int*, int*);
 void matrix_clear (int**, int);
 
 int main()
@@ -28,9 +28,9 @@ int main()
         matrix_manualfill (A, N);
     system("cls");
     matrix_print (A, N); // printing matrix
-    int increment = 1, max_temp=0;
-    matrix_find_bigger_values_below_main_diagonal (A, N, &increment, &max_temp); // looking for bigger numbers on and below main diagonal
-    matrix_print_bigger_values (A, N, &increment, &max_temp); // actually printing out all the numbers based on info from the last function
+    int increment = 1, max=0, i=0, j=0;
+    matrix_find_bigger_values_below_main_diagonal (A, N, &increment, &max, &i, &j); // looking for bigger numbers on and below main diagonal
+    matrix_print_bigger_values (A, N, &increment, &max, &i, &j); // actually printing out all the numbers based on info from the last function
     matrix_clear (A, N);
     return 0;
 }
@@ -123,53 +123,52 @@ void matrix_print (const int** A, int N)
     }
 }
 
-void matrix_find_bigger_values_below_main_diagonal (int** A, int N, int* increment, int* max_temp)
+void matrix_find_bigger_values_below_main_diagonal (int** A, int N, int* increment, int* max, int* pointer_i, int* pointer_j)
 {
-    int increment_temp=0, decrement = N-1, max_temp_temp=0;
+    int increment_temp=0, decrement = N-1, max_temp=0, temp_i=0, temp_j=0;
     for (int i=decrement; i>=0; i--)
     {
         for (int j=decrement; j>=0; j--)
         {
-            if (max_temp_temp==A[i][j])
+            if (max_temp==A[i][j])
                 increment_temp++;
-            else if (max_temp_temp<A[i][j])
+            else if (max_temp<A[i][j])
             {
-                max_temp_temp=A[i][j];
+                max_temp=A[i][j];
                 increment_temp=1;
+                temp_i = i;
+                temp_j = j;
             }
         }
         decrement--;
     }
     *increment = increment_temp;
-    *max_temp = max_temp_temp;
+    *max = max_temp;
+    *pointer_i = temp_i;
+    *pointer_j = temp_j;
 }
 
-void matrix_print_bigger_values (int** A, int N, int* increment_pointer, int* max_pointer)
+void matrix_print_bigger_values (int** A, int N, int* increment, int* max, int* i_pointer, int* j_pointer)
 {
     int search_value = 1, k = 1;
-    int increment_temp = *increment_pointer, increment = *increment_pointer, max_temp = *max_pointer;
-    for (int i=0; i<search_value; i++)
+    if (*increment==1)
+        printf("\nMax Value: [%d][%d] %d", *i_pointer+1, *j_pointer+1, *max);
+    else if (*increment>1)
     {
-        for (int j=0; j<search_value; j++)
+        for (int i=0; i<search_value; i++)
         {
-            if (max_temp==A[i][j] && increment>=0 && increment_temp!=1)
+            for (int j=0; j<search_value; j++)
             {
-                printf("\nMax Value #%d: [%d][%d] %d", k, i+1, j+1, max_temp);
-                if (increment==0)
-                    break;
-                increment--;
-                k++;
+                    printf("\nMax Value #%d: [%d][%d] %d", k, i+1, j+1, *max);
+                    if (*increment==0)
+                        break;
+                    *increment--;
+                    k++;
             }
-            else if (max_temp==A[i][j] && increment_temp==1)
-            {
-                printf("\nMax Value: [%d][%d] %d", i+1, j+1, max_temp);
-                increment_temp--;
+            if (*increment==0 || search_value==N)
                 break;
-            }
+            search_value++;
         }
-        if (increment==1 && increment_temp==0 || increment==0 || search_value==N)
-            break;
-        search_value++;
     }
 }
 
